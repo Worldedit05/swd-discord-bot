e4ab398c44ca6a40c0cf6e882fc5a61ffbb5a307
Aaron Lee <Worldedit05@gmail.com>
2018-05-25 18:35:08 -0500

    Convert HTML to MD for card details

--- helper/getDetailedCardEmbed.js
+++ helper/getDetailedCardEmbed.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const TurndownService = require('turndown')
+const turndownService = new TurndownService()
+
 function getDetailedCardEmbed(card) {
   let color;
   switch(card.faction_code) {
@@ -18,13 +21,14 @@ function getDetailedCardEmbed(card) {
   }
 
   var imageUrl = card.imagesrc || 'http://via.placeholder.com/298x418?text=No+Card+Image';
+  var cardDetails = turndownService.turndown(`${card.text}`);
 
   var embed = new Discord.RichEmbed()
     .setTitle(`${card.label}`)
     .setImage(imageUrl)
     .setURL(card.url)
     .setColor(color)
-    .setDescription(card.text)
+    .setDescription(cardDetails)
     .addField('Character Points', card.points === null ? 'N/A' : card.points, true)
     .addField('Cost', card.cost === null ? 'N/A' : card.cost, true)
     .addField('Affiliation', card.affiliation_name, true)
