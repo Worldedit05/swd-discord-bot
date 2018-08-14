3fcd1c7aaa615917c051ccc14fc527bb10ed3a2f
Aaron Lee <Worldedit05@gmail.com>
2018-08-14 01:59:13 -0500

    Cleaned up emoji replacer

---
+++ helper/emojiReplacer.js
@@ -0,0 +1,7 @@
+const emojiReplacer = (string) => {
+  let newString = string.replace(/\[/g, ':swd');
+  newString = newString.replace(/\]/g, ':');
+  return newString;
+}
+
+module.exports = emojiReplacer;
--- helper/getDetailedCardEmbed.js
+++ helper/getDetailedCardEmbed.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const emojiMapper = require('./symbolToEmojiMapper');
+const emojiReplacer = require('./emojiReplacer');
 const TurndownService = require('turndown')
 const turndownService = new TurndownService()
 
@@ -22,7 +22,7 @@ function getDetailedCardEmbed(card) {
   }
 
   var imageUrl = card.imagesrc || 'http://via.placeholder.com/298x418?text=No+Card+Image';
-  let cardDetails = turndownService.turndown(`${emojiMapper(card.text)}`);
+  let cardDetails = turndownService.turndown(`${emojiReplacer(card.text)}`);
 
   var embed = new Discord.RichEmbed()
     .setTitle(`${card.label}`)
