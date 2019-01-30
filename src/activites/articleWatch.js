f4b371239005e28d6199ac28e722813c47d86808
Aaron Lee <Worldedit05@gmail.com>
2019-01-30 14:50:09 -0600

    fix: article search string to reflect changes by FFG

--- src/activites/articleWatch.js
+++ src/activites/articleWatch.js
@@ -25,7 +25,7 @@ module.exports = (channel) => {
     let starWarsArticleLink = '';
     let isSavedArticle = false;
 
-    if (articleDescription.includes('Star Wars: Destiny')) {
+    if (articleDescription.includes('Star Wars') && articleDescription.includes('Destiny')) {
       starWarsArticleLink = item.link;
 
       try {
