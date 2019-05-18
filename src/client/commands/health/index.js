b1567c2eaca5ab401ba01d0aae40ce87f8ac04c9
Aaron Lee <Worldedit05@gmail.com>
2019-05-18 13:54:46 -0500

    fix: formatting

--- src/client/commands/health/index.js
+++ src/client/commands/health/index.js
@@ -1,17 +1,18 @@
 const healthCheck = (message) => {
-  if (process.env.PRODUCTION !== 'production') {
+  if (!process.env.HEROKU_SLUG_COMMIT) {
     require('child_process').exec('git rev-parse HEAD', function(err, stdout) {
       message.channel.send(`Current bot commit hash is: ${stdout.substr(stdout.length - 8)}`);
     });
   }
+  const healthText = `\`\`\`yaml
+AppID: ${process.env.HEROKU_APP_ID}
+App Name: ${process.env.HEROKU_APP_NAME}
+Release Created Date: ${process.env.HEROKU_RELEASE_CREATED_AT}
+Release Version: ${process.env.HEROKU_RELEASE_VERSION}
+Commit: ${process.env.HEROKU_SLUG_COMMIT}
+\`\`\``;
 
-  return message.channel.send(`
-    AppID: ${process.env.HEROKU_APP_ID}
-    App Name: ${process.env.HEROKU_APP_NAME}
-    Release Created Date: ${process.env.HEROKU_RELEASE_CREATED_AT}
-    Release Version: ${process.env.HEROKU_RELEASE_VERSION}
-    Commit: ${process.env.HEROKU_SLUG_COMMIT}
-    `);
+  return message.channel.send(healthText);
 };
 
 module.exports = healthCheck;
