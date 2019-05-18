709b3f45841fb6a713ebc1d0479db1b30d2630a9
Aaron Lee <Worldedit05@gmail.com>
2019-05-18 13:49:45 -0500

    fix: health check for prod

--- src/client/commands/health/index.js
+++ src/client/commands/health/index.js
@@ -1,7 +1,17 @@
 const healthCheck = (message) => {
-  require('child_process').exec('git rev-parse HEAD', function(err, stdout) {
-    message.channel.send(`Current bot commit hash is: ${stdout.substr(stdout.length - 8)}`);
-  });
+  if (process.env.PRODUCTION !== 'production') {
+    require('child_process').exec('git rev-parse HEAD', function(err, stdout) {
+      message.channel.send(`Current bot commit hash is: ${stdout.substr(stdout.length - 8)}`);
+    });
+  }
+
+  return message.channel.send(`
+    AppID: ${process.env.HEROKU_APP_ID}
+    App Name: ${process.env.HEROKU_APP_NAME}
+    Release Created Date: ${process.env.HEROKU_RELEASE_CREATED_AT}
+    Release Version: ${process.env.HEROKU_RELEASE_VERSION}
+    Commit: ${process.env.HEROKU_SLUG_COMMIT}
+    `);
 };
 
 module.exports = healthCheck;
